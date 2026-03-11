959d31cec4099af4099983059a8d4cdd373df186
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-03-11 15:04:58 +0100

    add validation

--- src/main/java/com/kvit/loader/ChunkBounds.java
+++ src/main/java/com/kvit/loader/ChunkBounds.java
@@ -1,6 +1,15 @@
 package com.kvit.loader;
 
 public record ChunkBounds(int minChunkX, int maxChunkX, int minChunkZ, int maxChunkZ) {
+    public ChunkBounds {
+        if (minChunkX > maxChunkX) {
+            throw new IllegalArgumentException("minChunkX (%d) > maxChunkX (%d)".formatted(minChunkX, maxChunkX));
+        }
+        if (minChunkZ > maxChunkZ) {
+            throw new IllegalArgumentException("minChunkZ (%d) > maxChunkZ (%d)".formatted(minChunkZ, maxChunkZ));
+        }
+    }
+
     public int minBlockX() {
         return this.minChunkX * 16;
     }
