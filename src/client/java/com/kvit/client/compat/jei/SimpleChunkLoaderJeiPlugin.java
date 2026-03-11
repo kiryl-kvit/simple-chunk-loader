ce4cb76e59f06ef164e1b42ca73c8fe60f70fb6a
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-03-11 18:05:50 +0100

    make JEI plugin work

---
+++ src/client/java/com/kvit/client/compat/jei/SimpleChunkLoaderJeiPlugin.java
@@ -0,0 +1,28 @@
+package com.kvit.client.compat.jei;
+
+import com.kvit.ModContent;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.runtime.IJeiRuntime;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.ItemStack;
+import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+
+@JeiPlugin
+public final class SimpleChunkLoaderJeiPlugin implements IModPlugin {
+	private static final Identifier UID = Identifier.fromNamespaceAndPath("simple-chunk-loader", "jei_plugin");
+
+	@Override
+	public @NonNull Identifier getPluginUid() {
+		return UID;
+	}
+
+	@Override
+	public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+		jeiRuntime.getIngredientManager()
+			.addIngredientsAtRuntime(VanillaTypes.ITEM_STACK, List.of(new ItemStack(ModContent.chunkLoaderItem())));
+	}
+}
