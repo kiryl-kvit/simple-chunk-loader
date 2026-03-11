231fca950a5123cb70d84ecb84a2f3825ff5c70f
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-03-11 14:51:45 +0100

    add item to menus

--- src/main/java/com/kvit/ModContent.java
+++ src/main/java/com/kvit/ModContent.java
@@ -4,12 +4,18 @@ import com.kvit.blocks.chunkLoader.ChunkLoaderBlock;
 import com.kvit.blocks.chunkLoader.entity.ChunkLoaderBlockEntity;
 import com.kvit.items.ChunkLoaderBlockItem;
 import eu.pb4.polymer.core.api.block.PolymerBlockUtils;
+import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -41,5 +47,21 @@ public final class ModContent {
 		CHUNK_LOADER_BLOCK_ENTITY = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, SimpleChunkLoader.id("chunk_loader"),
 			FabricBlockEntityTypeBuilder.create(ChunkLoaderBlockEntity::new, CHUNK_LOADER).build());
 		PolymerBlockUtils.registerBlockEntity(CHUNK_LOADER_BLOCK_ENTITY);
+
+		CreativeModeTab tab = PolymerItemGroupUtils.builder()
+			.title(Component.literal("Simple Chunk Loader"))
+			.icon(() -> new ItemStack(Items.LODESTONE))
+			.displayItems((params, output) -> {
+				output.accept(CHUNK_LOADER_ITEM);
+			})
+			.build();
+		PolymerItemGroupUtils.registerPolymerItemGroup(
+			SimpleChunkLoader.id("chunk_loader"),
+			tab
+		);
+
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries -> {
+			entries.accept(CHUNK_LOADER_ITEM);
+		});
 	}
 }
