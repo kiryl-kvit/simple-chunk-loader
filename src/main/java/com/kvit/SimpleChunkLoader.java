59f80b25422d7daaca2a30c7a80c341edb21e8cd
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-03-11 10:33:26 +0100

    init

---
+++ src/main/java/com/kvit/SimpleChunkLoader.java
@@ -0,0 +1,17 @@
+package com.kvit;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleChunkLoader implements ModInitializer {
+	public static final String MOD_ID = "simple-chunk-loader";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("{} has been initialized!", MOD_ID);
+	}
+}
